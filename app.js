6f92d33fa6d7e810f88177ed4e2616ec453288c0
jmhmd <jhostetter@gmail.com>
2013-04-17 14:59:29 +0000

    updated port

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express');
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 5000;
 var app = express();
  
 app.get('/', function(request, response) {
     response.sendfile(__dirname + '/app/index.html');
 }).configure(function() {
   app.use('/', express.static(__dirname + '/app'));
-}).listen(port);
+}).listen(port, function() {
+  console.log("Listening on " + port);
+});
